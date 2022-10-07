232908a87a1b67f94b81a204990ad8de11dbcb55
Thiago Balonyi <thiagocandal@gmail.com>
2022-10-06 21:30:41 -0300

    fix: Respostas certo e errado refresh auto

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 const form = document.getElementById('form-campos');
+const campoA = document.getElementById('campo-a');
+const campoB = document.getElementById('campo-b');
+const sucessMessage = 'CORRETO';
+const errorMessage = 'ERRADO';
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    const campoA = document.getElementById('campo-a');
-    const campoB = document.getElementById('campo-b');
-    const sucessMessage = 'CORRETO';
-    const errorMessage = 'ERRADO';
+    certoErrado();
+})
 
+function certoErrado() {
     if (campoB.value >= campoA.value) {
         const containerMensagemSucesso = document.querySelector('.sucess-message');
         containerMensagemSucesso.innerHTML = sucessMessage;
         containerMensagemSucesso.style.display = 'block';
+
+        const containerMensagemErro = document.querySelector('.error-message');
+        containerMensagemErro.innerHTML = errorMessage;
+        containerMensagemErro.style.display = 'none';
+
     } else {
         const containerMensagemErro = document.querySelector('.error-message');
         containerMensagemErro.innerHTML = errorMessage;
         containerMensagemErro.style.display = 'block';
+
+        const containerMensagemSucesso = document.querySelector('.sucess-message');
+        containerMensagemSucesso.innerHTML = sucessMessage;
+        containerMensagemSucesso.style.display = 'none';
     }
-})
+}
 
 console.log(form);
